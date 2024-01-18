d07ca68fb2c256fe4a72fcc832e66363b1a18c9a
June Lee <junelee@Junes-Laptop.local>
2024-01-17 22:31:14 -0600

    array exercises 1-6

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -1,10 +1,25 @@
 let holdCabinet1 = ['duct tape', 'gum', 3.14, false, 6.022e23];
 let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
-
+let bothCabinets = `${holdCabinet1} | ${holdCabinet2}`
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+holdCabinet1.concat(holdCabinet2);
+console.log(holdCabinet1);
+console.log(`array.concat() does not alter original arrays\n`)
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
-
+holdCabinet1.slice(2);
+holdCabinet2.slice(2);
+console.log(bothCabinets);
+console.log(`array.slice() does not alter original array\n`);
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse();
+console.log(holdCabinet1);
+console.log('array.reverse() alters orignal array\n');
+
+holdCabinet2.sort();
+console.log(`${holdCabinet2}
+array.sort() alters original array`);
+
+
--- arrays/exercises/part-two-arrays.js
+++ arrays/exercises/part-two-arrays.js
@@ -1,11 +1,31 @@
 let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
 
+let indexOfCargoHold = 0
+for (let i = 0; i < cargoHold.length; i++) {
+  console.log(`${cargoHold[i]} ${indexOfCargoHold++}`)
+}
 //1) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
+cargoHold[5] = 'space tether';
+console.log(cargoHold);
 
 //2) Remove the last item from the array with pop. Print the element removed and the updated array.
+let popCargoHold = cargoHold.pop();
+console.log(popCargoHold);
+console.log(cargoHold);
 
 //3) Remove the first item from the array with shift. Print the element removed and the updated array.
+let shiftCargoHold = cargoHold.shift();
+console.log(shiftCargoHold);
+console.log(cargoHold);
 
 //4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
+cargoHold.unshift(1138);
+cargoHold.push('20 meters');
+console.log(cargoHold);
 
-//5) Use a template literal to print the final array and its length.
+//5) Use a template literal to print the final array and its length
+console.log(`The Final Array: 
+
+${cargoHold} 
+
+Final Array length:  ${cargoHold.length}`)
