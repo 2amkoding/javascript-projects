84fbb8faf3e43e3aee5ab617a43d2e155b1d0b5c
June Lee <junelee@Junes-Laptop.local>
2024-01-30 22:53:10 -0600

    objects chp exercises completed

--- objects-and-math/chapter-examples/ForInLoop.js
+++ objects-and-math/chapter-examples/ForInLoop.js
@@ -3,7 +3,33 @@ let tortoiseOne = {
    name: "Pete",
    weight: 919,
    age: 85,
-   diet: ["pumpkins", "lettuce", "cabbage"]
+   diet: ["pumpkins", "lettuce", "cabbage"],
+   birthday: function() {
+      return this.age = this.age + 1;
+   }
 };
 
+let tortoiseTwo = {
+   species: "Galapagos Tortoise",
+   name: "Patricia",
+   weight: 800,
+   age: 85,
+   diet: ["pumpkins", "lettuce", "cabbage"],
+   sign: function() {
+      return this.name + " is a " + this.species;
+   }
+ };
 // Using a for..in loop, iterate through each property in the tortoiseOne object and print the value to the console.
+
+console.log(tortoiseOne.weight);
+tortoiseOne.weight = tortoiseOne.weight + 10;
+console.log(tortoiseOne["weight"]);
+console.log(tortoiseOne);
+for (item in tortoiseOne) {
+   console.log(`${item}: ${tortoiseOne[item]}`)
+}
+for (item in tortoiseTwo) {
+   console.log(`${item}: ${tortoiseTwo[item]}`)
+}
+
+console.log(tortoiseOne.birthday);
--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -10,8 +10,58 @@ function randomSelection(arr){
  for (i=0; i < 8; i++){
    console.log(randomSelection(happiness));
  }
- 
+ console.log('\n')
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
  //b) Have the code randomly pick one array, and then print 2 random items from it.
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+
+// console.log('\n')
+for (i = 0; i <3; i++) {
+// console.log(randomSelection(happiness));
+}
+// console.log('\n')
+let arrayOfWords = {
+  happiness: happiness,
+  words: words
+};
+// HOW TO SORT THRU OBJECTS THOUGH?
+function pickKeyFromObject (obj) {
+  return Object.keys(obj)[Math.floor(Math.random()*Object.keys(obj).length)];
+ 
+}
+  // function chooseArray (obj) {
+  //   let chosen = [];
+  //   for(item in obj) {
+      
+  //   }
+    
+  // }
+
+let arrWords = [happiness, words];
+//console.log(arrWords)
+
+
+function tookForever (arr, times) {
+  let chosenArr = randomSelection(arr);
+  for (let i=0; i < times; i++)
+  console.log(randomSelection(chosenArr));
+}
+
+function choose1FromArrs (arr1, arr2,) {
+  let pushed = [];
+  pushed.push(randomSelection(arr1));
+  pushed.push(randomSelection(arr2));
+  console.log(pushed);
+}
+choose1FromArrs(happiness, words, 1);
+
+ //  console.log(Math.trunc(-3.87))
+//  let num = Math.floor(Math.random()*10);
+
+//  console.log(num);
+ 
+//  for (i=0; i < 5; i++){
+//   let num = Math.random()*10;
+//   console.log(`floor = ${Math.floor(num)}, ceil = ${Math.ceil(num)}, round = ${Math.round(num)}`);
+// }
