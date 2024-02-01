dcd6665293c12b94845c3d4b13cd12e8956c8a85
June Lee <junelee@Junes-Laptop.local>
2024-02-01 12:06:50 -0600

    completed forreal. completed pt3

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -33,9 +33,9 @@ let gross = {
    age: 1
 };
 
-let allObjs = [superChimpOne, salamander,superChimpTwo, cowardlyDog, gross];
-
-allObjs.forEach((a)=>{a.astronautID = Math.floor(Math.random()*10)+1});
+let crew = [superChimpOne, salamander,superChimpTwo, cowardlyDog, gross];
+crew.forEach((a)=>{a.astronautID = Math.floor(Math.random()*10)+1});
+crew.forEach((a)=>{a.move = Math.floor(Math.random()*10)+1});
 
 // After you have created the other object literals, add the astronautID property to each one.
 
@@ -44,4 +44,33 @@ allObjs.forEach((a)=>{a.astronautID = Math.floor(Math.random()*10)+1});
 // Print out the relevant information about each animal.
 
 // Start an animal race!
-console.log(allObjs)
+
+console.log(crew);
+
+let crewReports = (obj) => (`${obj.name} is a ${obj.species}.  They are ${obj.age} years old and ${obj.mass} kilograms.  Their ID is ${obj.astronautID}`);
+console.log(crewReports(gross));
+
+function fitnessTest (ar) {
+  let newAr = [];
+   for (let i = 0; i < ar.length; i++) {
+   turns(ar[i]);
+   newAr.push(`${ar[i].name} took ${turns(ar[i])} turns to reach 20 steps`)
+  }
+  return newAr
+}
+
+
+function turns(obj) {
+   let steps = 0;
+   let turns = 0;
+   while (steps <=20){
+      steps+= obj.move;
+      turns++;
+   }
+   return turns
+}
+//console.log(gross)
+console.log(turns(gross))
+
+
+console.log(fitnessTest(crew))
