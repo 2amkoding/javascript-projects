fc133a08cace93eebbfb7fba465ad9d7270d1630
June Lee <junelee@Junes-Laptop.local>
2024-02-01 10:38:44 -0600

    Object exercises complete

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,6 +12,30 @@ let salamander = {
    age: 5
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6
+};
+
+let cowardlyDog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5
+};
+
+let gross = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1
+};
+
+let allObjs = [superChimpOne, salamander,superChimpTwo, cowardlyDog, gross];
+
+allObjs.forEach((a)=>{a.astronautID = Math.floor(Math.random()*10)+1});
 
 // After you have created the other object literals, add the astronautID property to each one.
 
@@ -20,3 +44,4 @@ let salamander = {
 // Print out the relevant information about each animal.
 
 // Start an animal race!
+console.log(allObjs)
