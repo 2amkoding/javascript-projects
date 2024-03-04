c20ccc2cf4ae88b7a306dfbb20d3c6cc927ffa74
June Lee <junelee@Junes-Laptop.local>
2024-03-03 20:29:42 -0600

    completed studio pt 1-2

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,28 @@
-// Write your JavaScript code here.
-// Remember to pay attention to page loading!
+function gitLit(){
+const takeoff = document.getElementById("takeoff");
+const flightStatus = document.getElementById("flightStatus");
+const shuttleBackground = document.getElementById("shuttleBackground");
+const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+const land = document.getElementById("landing"); 
+
+takeoff.addEventListener('click', event =>{
+    let response  =window.confirm("Confirm that the shuttle is ready for takeoff.");
+    
+    if(response){
+        flightStatus.innerHTML = "Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = "blue";
+        spaceShuttleHeight.innerHTML = 10000;
+    }
+});
+
+land.addEventListener('click', event => {
+    let response = window.confirm("The shuttle is landing. Landing gear engaged.");
+
+    if(response){
+        flightStatus.innerHTML = "The shuttle has landed.";
+    }
+})
+
+}
+window.addEventListener("load", gitLit);
