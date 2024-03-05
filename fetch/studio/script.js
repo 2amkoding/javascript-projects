cdef9a891b54de106628532c18cb08338d2e1b6a
June Lee <junelee@Junes-Laptop.local>
2024-03-04 20:18:50 -0600

    finish studio later

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,10 @@
 //TODO: Add Your Code Below
+ 
+<script>
+
+        window.addEventListener("load", function() {
+            fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+                console.log(response);
+            } );
+        });
+</script>
